129680813df76165c309a1386430d47f711858c4
s13g <ayflix0@gmail.com>
2022-10-29 17:58:16 +0100

    added additional packages

--- cs50fanpage/settings.py
+++ cs50fanpage/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'cs50fanpage.herokuapp.com']
 
 # Application definition
 
